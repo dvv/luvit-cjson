664910709b6fd44ea64eed90778a1687229b85fb
Vladimir Dronnikov <dronnikov@gmail.com>
2012-05-16 21:45:16 +0400

    streamin

--- init.lua
+++ init.lua
@@ -3,6 +3,35 @@ _G.cjson = nil
 
 local parse = JSON.decode
 
+local function streamingParser(callback, options)
+  local self = {}
+  self.options = options
+  self.parse = function(self, str)
+    while true do
+      local status, result = pcall(parse, str)
+      if status then
+        callback(result)
+        return
+      end
+    end
+    --print(result, str)
+    if result:find('Expected the end ', nil, true) == 1 then
+      local at = result:find(' at character ', nil, true)
+      if at then
+        at = tonumber(result:sub(at + 14))
+        if at then
+          callback(parse(str:sub(1, at - 1)))
+          str = str:sub(at)
+        end
+      end
+    else
+      error(result)
+    end
+  end
+  --
+  return self
+end
+
 local function parseMany(str)
   local results = {}
   while true do
@@ -21,6 +50,8 @@ local function parseMany(str)
           str = str:sub(at)
         end
       end
+    else
+      error(result)
     end
   end
 end
@@ -29,5 +60,6 @@ return {
   stringify = JSON.encode,
   parse = parse,
   parseMany = parseMany,
+  streamingParser = streamingParser,
   null = JSON.null,
 }
